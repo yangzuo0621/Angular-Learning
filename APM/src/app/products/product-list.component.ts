48332d6355835e831427aecfaf5a45418ccb6b90
Zuo Yang <zuya@microsoft.com>
2018-03-25 09:37:18 +0800

    data binding and pipe

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -5,5 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html'
 })
 export class ProductListComponent {
+  pageTitle = 'Product List';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+  listFilter = 'cart';
+  products: any[] = [
+    {
+      'productId': 2,
+      'productName': 'Garden Cart',
+      'productCode': 'GDN-0023',
+      'releaseDate': 'March 18, 2016',
+      'description': '15 gallon capacity rolling garden cart',
+      'price': 32.99,
+      'starRating': 4.2,
+      'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
+    },
+    {
+      'productId': 5,
+      'productName': 'Hammer',
+      'productCode': 'TBX-0048',
+      'releaseDate': 'May 21, 2016',
+      'description': 'Curved claw steel hammer',
+      'price': 8.9,
+      'starRating': 4.8,
+      'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png'
+    }
+  ];
 
+  public toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
