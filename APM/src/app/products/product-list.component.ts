0663d79779f41874cc6aa16ee348ce64eeb1263b
Zuo Yang <zuya@microsoft.com>
2018-03-25 12:17:34 +0800

    add angular service

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-products',
@@ -22,36 +23,14 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      'productId': 2,
-      'productName': 'Garden Cart',
-      'productCode': 'GDN-0023',
-      'releaseDate': 'March 18, 2016',
-      'description': '15 gallon capacity rolling garden cart',
-      'price': 32.99,
-      'starRating': 4.2,
-      'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
-    },
-    {
-      'productId': 5,
-      'productName': 'Hammer',
-      'productCode': 'TBX-0048',
-      'releaseDate': 'May 21, 2016',
-      'description': 'Curved claw steel hammer',
-      'price': 8.9,
-      'starRating': 4.8,
-      'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png'
-    }
-  ];
+  products: IProduct[] = [];
 
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFilter = 'cart';
+  constructor(private _productService: ProductService) {
   }
 
   ngOnInit(): void {
-    console.log('In Onit');
+    this.products = this._productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
   private performFilter(filterBy: string): IProduct[] {
