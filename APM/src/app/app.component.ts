904c771cc4a11664d73eaa7004dfa8278701d9e6
Zuo Yang <zuya@microsoft.com>
2018-03-25 15:11:32 +0800

    navbar and routing

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -3,12 +3,7 @@ import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'pm-root',
-  template: `
-    <div>
-      <h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
-    </div>
-  `,
+  templateUrl: './app.component.html',
   providers: [ ProductService ]
 })
 export class AppComponent {
