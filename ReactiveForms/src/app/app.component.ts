e6eadf8aee60e364b85982b55b6159e8ea348b9a
Zuo Yang <zuya@microsoft.com>
2018-03-27 13:42:26 +0800

    Initialize start angular form project

---
+++ ReactiveForms/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="container">
+      <app-signup></app-signup>
+    </div>
+  `
+})
+export class AppComponent { }
