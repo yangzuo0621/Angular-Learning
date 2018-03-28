d8fc86451ea9df5311deaac2616cd0262013e6c4
Zuo Yang <zuya@microsoft.com>
2018-03-28 09:22:50 +0800

    adjust validation rules at runtime

--- ReactiveForms/src/app/customers/customer.component.ts
+++ ReactiveForms/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
@@ -15,9 +15,11 @@ export class CustomerComponent implements OnInit {
 
   ngOnInit(): void {
     this.customerForm = this.formBuilder.group({
-      firstName: '',
-      lastName: '',
-      email: '',
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]],
+      phone: '',
+      notification: 'email',
       sendCatalog: true
     });
   }
@@ -25,6 +27,16 @@ export class CustomerComponent implements OnInit {
   public save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  public setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity(); // Must Needed
   }
 
   public populateTestData(): void {
